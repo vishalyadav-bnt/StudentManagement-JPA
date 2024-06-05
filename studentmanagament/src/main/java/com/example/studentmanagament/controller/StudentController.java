1d6137c82dd70c73f668b399e9ee2a7db32295fc
Vishal Yadav <vishal.yadav@bnt.local>
2024-06-05 11:53:10 +0530

    implemented logger in project

--- studentmanagament/src/main/java/com/example/studentmanagament/controller/StudentController.java
+++ studentmanagament/src/main/java/com/example/studentmanagament/controller/StudentController.java
@@ -3,6 +3,7 @@ package com.example.studentmanagament.controller;
 import java.util.List;
 import java.util.UUID;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,17 +21,21 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.studentmanagament.model.StudentModel;
 import com.example.studentmanagament.response.SuccessResponnse;
 import com.example.studentmanagament.service.StudentServiceimpl;
-
+import org.slf4j.Logger;
 
 @RestController
 @RequestMapping("v1/student")
 public class StudentController {
+        private static final Logger logger = LoggerFactory.getLogger(StudentController.class);
+
     @Autowired
     StudentServiceimpl studentServiceimpl;
 
     @PostMapping("/")
     public ResponseEntity<SuccessResponnse> save(@RequestBody StudentModel studentModel) {
+        logger.info("Received request to save student data");
         StudentModel studentModel2 = studentServiceimpl.save(studentModel);
+        logger.info("Student data saved successfully");
         SuccessResponnse successResponnse=new SuccessResponnse("Data Store Succesfully...",HttpStatus.CREATED.value(),studentModel2);
         return new ResponseEntity<>(successResponnse,HttpStatus.CREATED);
     }
@@ -38,29 +43,35 @@ public class StudentController {
     
     @GetMapping
     public ResponseEntity<SuccessResponnse>getAllStudent(){
+        logger.info("Recieved Request for fetch all data");
         List<StudentModel>list=studentServiceimpl.getAll();
+        logger.info("Fetch All Data successfully...");
         SuccessResponnse successResponnse=new SuccessResponnse("Data Fetch Succesfully...",HttpStatus.OK.value(),list);
         return new ResponseEntity<>(successResponnse,HttpStatus.OK);
     }
 
     @DeleteMapping("{id}")
     public ResponseEntity<String>deleteStudentById(@PathVariable("id") UUID id)
-    {System.out.println("sdsad");
+    {  logger.info("Received request for delete data  with id :{}",id);
        studentServiceimpl.deleteStudentById(id);
+       logger.info("Data Deleted Succesfully with id {}", id);
        return ResponseEntity.ok("Data Deleted Succesfully");
     }
 
     @PutMapping("{id}")
     public ResponseEntity<SuccessResponnse>updateStudentRecordById(@PathVariable("id")UUID id,@RequestBody StudentModel studentModel)
-    {
+    {   logger.info("Received request for update data  with id :{}",id);
         StudentModel studentModel2=studentServiceimpl.updateStudentById(id,studentModel);
+        logger.info("Update  data  with id :{}",id);
         SuccessResponnse successResponnse=new SuccessResponnse("Data Updated Succesfully",HttpStatus.OK.value(),studentModel2);
         return new ResponseEntity<>(successResponnse,HttpStatus.OK);
     }
 
     @PatchMapping("{id}")
     public ResponseEntity<SuccessResponnse>updateStudentNameById(@PathVariable("id")UUID id,@RequestParam("name")String name){
+        logger.info("Received request for update data  with id :{}",id);
         StudentModel studentModel=studentServiceimpl.updateStudentNameById(id,name);
+        logger.info("Update data  with id :{}",id);
         SuccessResponnse successResponnse=new SuccessResponnse("Name Updated succesfully",HttpStatus.OK.value(),studentModel);
         return new ResponseEntity<>(successResponnse,HttpStatus.OK);
     }
--- studentmanagament/src/main/java/com/example/studentmanagament/service/StudentServiceimpl.java
+++ studentmanagament/src/main/java/com/example/studentmanagament/service/StudentServiceimpl.java
@@ -3,48 +3,57 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
+import com.example.studentmanagament.controller.StudentController;
 import com.example.studentmanagament.exception.DataIsNotPresent;
 import com.example.studentmanagament.exception.StudentIdIsNotFound;
 import com.example.studentmanagament.exception.StudentObjectISNull;
 import com.example.studentmanagament.model.StudentModel;
 import com.example.studentmanagament.repositiory.StudentRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Service
 public class StudentServiceimpl implements StudentServices {
+    private static final Logger logger = LoggerFactory.getLogger(StudentController.class);
+
     @Autowired
     StudentRepository studentRepository;
     @Override
     public StudentModel save(StudentModel studentModel) {
+      logger.info("Saving student data {}",studentModel);
        if(studentModel==null)
-       {
+       {logger.error("Object is null");
         throw new StudentObjectISNull("Student Object Is Null");
        }
+       logger.info("Student data stored succesfully", studentModel);
        return studentRepository.save(studentModel);
     }
     @Override
     public void deleteStudentById(UUID id) {
+      logger.info("Deleting Data with {}",id);
        Optional<StudentModel> studentOptional = studentRepository.findById(id);
        if(studentOptional!=null&& !studentOptional.isEmpty())
-       {
+       {  logger.info("Data Deleted with id:{}",id);
           studentRepository.deleteById(id);
        }else
-       {
+       { logger.error("Id not foud :{}",id);
             throw new StudentIdIsNotFound("Student id is not present");
        }
     }
     @Override
     public List<StudentModel> getAll() {
+      logger.info("Fetching All Data.....");
        if(studentRepository.findAll().isEmpty())
-       {
+       {logger.error("Data Is not present in database for fetching");
         throw new DataIsNotPresent("Data is not present");
        }
+       logger.info("Fetch All Data");
        return studentRepository.findAll();
     }
    @Override
    public StudentModel updateStudentById(UUID id, StudentModel studentModel) {
+      logger.info("Uodating data with id:{}",id);
       Optional<StudentModel>stuOptional=studentRepository.findById(id);
       if(stuOptional.isPresent())
       {
@@ -52,22 +61,25 @@ public class StudentServiceimpl implements StudentServices {
          studentModel2.setName(studentModel.getName());
          studentModel2.setCity(studentModel.getCity());
          studentModel2.setClassroom(studentModel.getClassroom());
+         logger.info("Data Updated SuccesFully :{}", studentModel2);
          return studentRepository.save(studentModel2);
       }
-      else{
+      else{logger.error("Id not foud :{}",id);
          throw new StudentIdIsNotFound("Id Not Found For Uodate StudentData");
       }
    }
    @Override
    public StudentModel updateStudentNameById(UUID id, String name) {
+      logger.info("Uodating data with id:{}",id);
       Optional<StudentModel>stuOptional=studentRepository.findById(id);
       if(stuOptional.isPresent())
       {
          StudentModel studentModel2=stuOptional.get();
          studentModel2.setName(name);
+         logger.info("Data Updated SuccesFully :{}", studentModel2);
          return studentRepository.save(studentModel2);
       }
-      else{
+      else{logger.error("Id not foud :{}",id);
          throw new StudentIdIsNotFound("Id Not Found For Uodate StudentData");
       }
    }
